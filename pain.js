cc8b6d01b9a89457c5c33f99f7542bf5cc88f50f
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-08-27 20:54:31 +0700

    Update pain.js

--- pain.js
+++ pain.js
@@ -3,12 +3,13 @@ export async function main(ns) {
   const host = ns.getHostname();
   const target = ns.args[0];
   const maxmoney = ns.getServerMaxMoney(target);
+  const minsecurity = ns.getServerMinSecurityLevel(target);
+  let money = ns.getServerMoneyAvailable(target);
+  let currentsec = ns.getServerSecurityLevel(target);
 
   while (true) {
     if (currentsec > minsecurity) {
-      let currentsec = ns.getServerSecurityLevel(target);
-      const minsecurity = ns.getServerMinSecurityLevel(target);
-      const threads = Math.ceil( ns.growthAnalyze(target,maxmoney / money, 1));
+      const threads = Math.ceil((currentsec - minsecurity) / ns.weakenAnalyze(1));
       const weakenram = ns.getScriptRam("weaken.js");
       const needram = Math.ceil(weakenram * threads);
       const weakentime = ns.formulas.hacking.weakenTime(ns.getServer(target), ns.getPlayer()) + 10;
@@ -25,7 +26,7 @@ export async function main(ns) {
         }
       };
     } else if (money < maxmoney) {
-      const threads = math.celi(ns.growthAnalyze(target, 1, 1));
+      const threads = Math.ceil( ns.growthAnalyze(target,maxmoney / money, 1));
       const growram = ns.getScriptRam("grow.js");
       var needram = Math.ceil(growram * threads);
       var growtime = ns.formulas.hacking.growTime(ns.getServer(target), ns.getPlayer()) + 10;
@@ -42,12 +43,10 @@ export async function main(ns) {
         }
       }
     } else {
-      var currentsec = ns.getServerSecurityLevel(target);
-      var minsecurity = ns.getServerMinSecurityLevel(target);
-      var threads = Math.celi(ns.hackAnalyzeThreads(target,maxmoney*0.75));
-      var hackram = ns.getScriptRam("hack.js");
-      var needram = Math.ceil(hackram * threads);
-      var hacktime = ns.formulas.hacking.hackTime(ns.getServer(target), ns.getPlayer()) + 10;
+      const threads = Math.celi(ns.hackAnalyzeThreads(target,maxmoney*0.75));
+      const hackram = ns.getScriptRam("hack.js");
+      const needram = Math.ceil(hackram * threads);
+      const hacktime = ns.formulas.hacking.hackTime(ns.getServer(target), ns.getPlayer()) + 10;
       ns.print("Need ram for hack:" + ns.formatRam(needram))
       for (const server of ns.scan(host)) {
         if (ns.getServerMaxRam(server) - ns.getServerUsedRam(server) >= needram) {
