fac345aaaa3aa75bccdf6609c66170204d03adc2
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-09-06 23:29:33 +0700

    Update script-monitor.js

--- script-monitor.js
+++ script-monitor.js
@@ -1,20 +1,22 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  let port = ns.getPortHandle(ns.getRunningScript("pain.js").pid)
-  let serverport = ns.getPortHandle(ns.getRunningScript("pain.js").pid + 5)
-  const lines = ["|        ","||       ","|||      ","||||     ","|||||    ","||||||   ","|||||||  ","|||||||| ","|||||||||"]
+  const arg = ns.args[0]
+  const home = "home"
+  let port = ns.getPortHandle(ns.getRunningScript("pain.js", home, arg).pid)
+  let serverport = ns.getPortHandle(ns.getRunningScript("pain.js", home, arg).pid + 5)
+  const lines = ["|        ", "||       ", "|||      ", "||||     ", "|||||    ", "||||||   ", "|||||||  ", "|||||||| ", "|||||||||"]
   let indexline = 0
   const refreshRate = 1000
   let time = 0
   const server = JSON.parse(serverport.read())
   ns.tail();
   ns.disableLog("sleep")
-  
+
   while (true) {
-    let serverCurrentSecurity = ns.getServerSecurityLevel(server)
-    const serverMinSecurity = ns.getServerMinSecurityLevel(server);
-    let servermoney = ns.formatNumber(ns.getServerMoneyAvailable(server),3,1000)
-    let servermaxmoney = ns.formatNumber(ns.getServerMaxMoney(server),3,1000)
+    let serverCurrentSecurity = ns.getServerSecurityLevel(arg)
+    const serverMinSecurity = ns.getServerMinSecurityLevel(arg);
+    let servermoney = ns.formatNumber(ns.getServerMoneyAvailable(arg), 3, 1000)
+    let servermaxmoney = ns.formatNumber(ns.getServerMaxMoney(arg), 3, 1000)
     let line = lines[indexline]
     ns.print("=============================================");
     ns.print(JSON.parse(port.peek()))
@@ -25,7 +27,7 @@ export async function main(ns) {
     ns.print("---------------------------------------------");
     ns.print("Elapsed for: " + ns.tFormat(time), "[" + line + "]")
     ns.print("=============================================");
-    
+
     await ns.sleep(refreshRate)
     ns.clearLog()
 
