dde92aeafc4befb9fc6cc73e3feee9cd0a55f532
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-09-15 23:15:20 +0700

    Update script-monitor.js

--- script-monitor.js
+++ script-monitor.js
@@ -4,12 +4,13 @@ export async function main(ns) {
   const home = "home"
   let port = ns.getPortHandle(ns.getRunningScript("pain.js", home, arg).pid)//Get port from batch script
   let port2 = ns.getPortHandle(1) //Addition port for cycle log
-  const lines = ["|        ", "||       ", "|||      ", "||||     ", "|||||    ", "||||||   ", "|||||||  ", "|||||||| ", "|||||||||"]
-  let indexline = 0 //Cool line beside time
+  const maxWidth = 10;
+  let indexline = 0;
   const refreshRate = 1000 //How often the loop refresh
   let time = 0 //Time for how long monitor have been running
   const edge = "============================================="
   const section = "---------------------------------------------"
+  const start = Date.now();
   ns.tail();
   ns.disableLog("sleep")
   ns.disableLog("getServerSecurityLevel")
@@ -18,13 +19,14 @@ export async function main(ns) {
   ns.disableLog("getServerMaxMoney")
 
   while (true) {
+    let time = Date.now() - start;
+    const line = "".padStart(indexline, "|").padEnd(maxWidth, " ");
     let serverCurrentSecurity = ns.getServerSecurityLevel(arg)
     let serverMinSecurity = ns.getServerMinSecurityLevel(arg)
     let servermoney = ns.formatNumber(ns.getServerMoneyAvailable(arg), 0, 1000)
     let servermaxmoney = ns.formatNumber(ns.getServerMaxMoney(arg), 0, 1000)
-    let money = `${servermoney}`.padStart(9)+"/"+`${servermaxmoney}`.padEnd(9)
-    let Security = "(".padStart(9) + serverMinSecurity + ")"  + Math.abs(serverCurrentSecurity - serverMinSecurity) + "+".padEnd(9)
-    let line = lines[indexline]
+    let money = `${servermoney}`.padStart(9) + "/" + `${servermaxmoney}`.padEnd(9)
+    let Security = "(".padStart(9) + serverMinSecurity + ")" + Math.abs(serverCurrentSecurity - serverMinSecurity) + "+".padEnd(9)
     ns.print(edge);
     ns.print("Target: " + arg)
     ns.print("pain.js: " + JSON.parse(port.peek()))
@@ -41,12 +43,9 @@ export async function main(ns) {
       ns.print("[" + "Empty :(" + "]")
     }
     ns.print(edge);
+    indexline = ++indexline % (maxWidth + 1);
 
     await ns.sleep(refreshRate)
     ns.clearLog()
-
-    indexline = indexline >= lines.length - 1 ? 0 : indexline + 1;
-
-    time = time + 1000
   }
 }
