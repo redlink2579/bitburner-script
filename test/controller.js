c74aa6ffd7549935a7e7287efe0e62060e25534b
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-11-27 12:04:19 +0700

    Update controller.js

--- test/controller.js
+++ test/controller.js
@@ -1,29 +1,39 @@
+const batch = [
+  { job: "hack", tool: "test/hack.js", ramcost: 1.7, delay: 0 },
+  { job: "weaken", tool: "test/weaken.js", ramcost: 1.75, delay: 1 },
+  { job: "grow", tool: "test/grow.js", ramcost: 1.75, delay: 2 },
+  { job: "weaken2", tool: "test/weaken.js", ramcost: 1.75, delay: 3 }
+]
+const yoink = 0.99
+
 import { getserver, isPrepped, GrantRoot, issecurityprep } from "lib/function.js"
 /** @param {NS} ns */
 export async function main(ns) {
   ns.tail()
   let servers = getserver(ns)
   servers.sort((x, y) => ns.getServerMaxMoney(y) - ns.getServerMaxMoney(x)) //Sorting targets by money
-  const targets = servers.filter(server => Math.floor(ns.getHackingLevel() * 0.75) > ns.getServerRequiredHackingLevel(server)) //filtering targets to only those that we can hack
+  const targets = servers.filter(server => Math.floor(ns.getHackingLevel() * 0.5) > ns.getServerRequiredHackingLevel(server)) //filtering targets to only those that we can hack
   const target = targets[0]
-  let ramhost = servers.filter(server => ns.hasRootAccess(server) == true) //Finding servers that we have access
-  ramhost.sort((x, y) => (ns.getServerMaxRam(x) - ns.getServerUsedRam(x)) - (ns.getServerMaxRam(y) - ns.getServerUsedRam(y)))//wait should I sort it's like this?
+  let weakentime = ns.getWeakenTime(target)
+  let growtime = ns.getGrowTime(target)
+  let hacktime = ns.getHackTime(target)
+  let ramhosts = getserver(ns)
+  let ramhost = ramhosts.filter(server => ns.hasRootAccess(server) == true) //Finding servers that we have access
+  ramhost.sort((x, y) => ns.getServerMaxRam(y) - ns.getServerMaxRam(x))//wait should I sort it's like this?
   JSON.stringify(ramhost)
-  if (ns.hasRootAccess(target) != true) {
+  ns.tprint(ramhost)
+  if (!ns.hasRootAccess(target)) {
     GrantRoot(ns, target)
   };
   if (!isPrepped(ns, target)) {
     if (issecurityprep(ns, target) == true) {
       const WT = Math.ceil((ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target)) / ns.weakenAnalyze(1))
-      ns.print(WT)
-      for (let i = 0; i <= ramhost.lenght; i++) {
-        if (10000 * WT < ns.getServerMaxRam(ramhost[i]) == true) {
-          let pid = ns.exec(Script[2], ramhost[i], WT, Delay[0], target)
-          let port = ns.getPortHandle(pid)
-          await port.nextWrite()
-        } else {
-          throw new Error("Not even enough ram on any to run a single weaken job!, upgrade ram or buy more servers to fix this")
-        }
+      let weaken = batch[1]
+      for (let i = 0; i++;) {
+        let pid = ns.exec(weaken.tool, ramhost[i], WT, weaken, weakentime)
+        let port = ns.getPortHandle(pid)
+        await port.nextWrite()
+        break
       }
     }
   }
