9085312757b64d0baf17e861055e56d76f432210
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-11-30 17:17:26 +0700

    Update hack.js

--- test/hack.js
+++ test/hack.js
@@ -2,22 +2,22 @@
 export async function main(ns) {
   const works = JSON.parse(ns.args[0])
   const target = ns.args[1]
-  let growtime = ns.args[2]
+  let weakentime = ns.args[2]
   let delay = ns.args[3]
   const port = ns.getPortHandle(ns.pid)
-  
+
   if (delay < 0) {
-    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(growtime))
+    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(weakentime))
     port.write(-delay)
     delay = 0
   } else {
     ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(delay))
-    await ns.grow(target, { additionalMsec: delay })
+    await ns.weaken(target, { additionalMsec: delay })
   }
 
-    const end = Date.now
-    ns.atExit(() =>
-      port.write(works, end),
-      ns.tprint("Successfully run ", works.job, " at:", target)
-    )
-  }
+  const end = Date.now
+  ns.atExit(() =>
+    port.write(works, end),
+    ns.tprint("Batch ", works, " has finished at ", ns.tFormat(end))
+  )
+}
