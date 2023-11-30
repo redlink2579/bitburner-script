de27375eeaa1a58cef079136bdeb061c6538b1ec
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-11-30 17:54:20 +0700

    change to actual grow.js not weaken.js

--- test/grow.js
+++ test/grow.js
@@ -2,23 +2,23 @@
 export async function main(ns) {
   const works = JSON.parse(ns.args[0])
   const target = ns.args[1]
-  let weakentime = ns.args[2]
+  let growtime = ns.args[2]
   let endtime = ns.args[3]
   const port = ns.getPortHandle(ns.pid)
-  let delay = endtime - Date.now() - weakentime
+  let delay = endtime - Date.now() - growtime
 
   if (delay < 0) {
-    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(weakentime))
+    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(growtime))
     port.write(-delay)
     delay = 0
   } else {
-    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(weakentime + delay))
-    await ns.weaken(target, { additionalMsec: delay })
+    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(growtime + delay))
+    await ns.grow(target, { additionalMsec: delay })
   }
 
-  const end = Date.now
-  ns.atExit(() =>
-    port.write(works, end),
-    ns.tprint("Batch ", works, " has finished at ", ns.tFormat(end))
-  )
-}
+    const end = Date.now
+    ns.atExit(() =>
+      port.write(works, end),
+      ns.tprint("Successfully run ", works.job, " at:", target)
+    )
+  }
