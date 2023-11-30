50cba2e511ff35d8669a7c41fca11d689ebd27a7
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-11-30 16:14:19 +0700

    Update weaken.js

--- test/weaken.js
+++ test/weaken.js
@@ -1,21 +1,26 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  const works = ns.args[0]
+  const works = JSON.parse(ns.args[0])
+  JSON.parse(works)
   const target = ns.args[1]
   let weakentime = ns.args[2]
-  let delay = works.delay
   const port = ns.getPortHandle(ns.pid)
-  delay = works.delay * 5
-  if (weakentime - Date.now < -delay) {
-    ns.tprint("!!WARNING!!:", works.job, " was " , ns.tFormat(-delay, true), "ms late, ", weakentime)
+  const startTime = Date.now();
+  const endTime = Date.now() + weakentime;
+  let timeElapsed = Date.now() - startTime;
+  let delay = endTime - weakentime + works.delay - Date.now()
+  if (delay < 0) {
+    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(weakentime))
     port.write(-delay)
     delay = 0
   } else {
-    await ns.weaken(target , { additionalMsec: delay })
-    const end = Date.now
+    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(weakentime))
+    await ns.weaken(target, { additionalMsec: delay })
   }
 
-  ns.atExit(() => 
-  ns.tprint ("Batch ", works.job, " has finished at ", ns.tFormat(end))
+  const end = Date.now
+  ns.atExit(() =>
+    port.write(works, end),
+    ns.tprint("Batch ", works, " has finished at ", ns.tFormat(end))
   )
 }
