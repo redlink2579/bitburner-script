3e16b43797b4741ecea46342d1355b1a5604a25d
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-11-30 17:17:14 +0700

    Update hack.js

--- test/hack.js
+++ test/hack.js
@@ -1,26 +1,23 @@
 /** @param {NS} ns */
 export async function main(ns) {
   const works = JSON.parse(ns.args[0])
-  JSON.parse(works)
   const target = ns.args[1]
-  let hacktime = ns.args[2]
+  let growtime = ns.args[2]
+  let delay = ns.args[3]
   const port = ns.getPortHandle(ns.pid)
-  const startTime = Date.now();
-  const endTime = Date.now() + works.hacktime;
-  let timeElapsed = Date.now() - startTime;
-  let delay = endTime - hacktime + works.delay - Date.now()
+  
   if (delay < 0) {
-    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(hacktime))
+    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(growtime))
     port.write(-delay)
     delay = 0
   } else {
-    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(hacktime))
-    await ns.weaken(target, { additionalMsec: delay })
+    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(delay))
+    await ns.grow(target, { additionalMsec: delay })
   }
 
-  const end = Date.now
-  ns.atExit(() =>
-    port.write(works, end),
-    ns.tprint("Batch ", works, " has finished at ", ns.tFormat(end))
-  )
-}
+    const end = Date.now
+    ns.atExit(() =>
+      port.write(works, end),
+      ns.tprint("Successfully run ", works.job, " at:", target)
+    )
+  }
