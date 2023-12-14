e5fd3fa7d53d7a949889b7b8ca291477205eb663
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2023-12-14 19:23:24 +0700

    Update weaken.js

--- test/weaken.js
+++ test/weaken.js
@@ -2,25 +2,26 @@
 export async function main(ns) {
   const works = JSON.parse(ns.args[0])
   const target = ns.args[1]
-  let weakentime = ns.args[2]
+  let growtime = ns.args[2]
   let endtime = ns.args[3]
   const dataport = ns.getPortHandle(ns.args[4])
   const port = ns.getPortHandle(ns.pid)
-  let delay = endtime - Date.now() - weakentime
+  let delay = endtime - Date.now() - growtime
+  ns.print(ns.tFormat(delay))
 
   if (delay < 0) {
-    ns.tprint("!!WARNING!!:", works.job, " was ", -delay, "ms late, expect runing time was:", ns.tFormat(weakentime))
+    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(growtime))
     port.write(-delay)
     delay = 0
   } else {
-    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(weakentime + delay))
+    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(growtime + delay))
     port.write(0)
   }
-  await ns.weaken(target, { additionalMsec: delay })
+  await ns.grow(target, { additionalMsec: delay })
 
-  const end = Date.now()
+  const end = Date.now
   ns.atExit(() => {
-    if (works.report) { dataport.write(works.job, end) };
-    ns.tprint("Batch ", works, " has finished at ", ns.tFormat(end))
+    if (works.report) { dataport.write(works.job, end) }
+    ns.tprint("Batch ", works.job, " has finished at ", new Date().toLocaleTimeString())
   })
 }
