93f5a9807d5d59c2e5a352e7466398224f504654
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2024-01-08 15:34:59 +0700

    Update weaken.js

--- test/weaken.js
+++ test/weaken.js
@@ -2,26 +2,26 @@
 export async function main(ns) {
   const works = JSON.parse(ns.args[0])
   const target = ns.args[1]
-  let growtime = ns.args[2]
+  let weakentime = ns.args[2]
   let endtime = ns.args[3]
   const dataport = ns.getPortHandle(ns.args[4])
   const port = ns.getPortHandle(ns.pid)
-  let delay = endtime - Date.now() - growtime
-  ns.print(ns.tFormat(delay))
+  let delay = endtime - Date.now() - weakentime
 
   if (delay < 0) {
-    ns.tprint("!!WARNING!!:", works.job, " was ", ns.tFormat(-delay, true), "late", ns.tFormat(growtime))
+    ns.tprint(`WARN: ${works.job} was ${-delay} ms late, expect runing time was:, ${ns.tFormat(weakentime)}`)
     port.write(-delay)
     delay = 0
   } else {
-    ns.tprint("Running ", works.job, " at ", target, " for:", ns.tFormat(growtime + delay))
+    ns.tprint(`INFO: Running ${works.job} at ${target} for: ${ns.tFormat(weakentime + delay)}`)
     port.write(0)
   }
-  await ns.grow(target, { additionalMsec: delay })
+  const secreduce = await ns.weaken(target, { additionalMsec: delay })
+  ns.write("logging.txt", "sec-reduce = " + secreduce + "\n", "a")
 
   const end = Date.now
   ns.atExit(() => {
     if (works.report) { dataport.write(works.job, end) }
-    ns.tprint("Batch ", works.job, " has finished at ", new Date().toLocaleTimeString())
+    ns.tprint(`SUCCESS: Batch ${works.job} has finished at  ${new Date().toLocaleTimeString()}`)
   })
 }
