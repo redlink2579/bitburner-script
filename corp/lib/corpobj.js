2963e69c1148f3db9eab96ffc2fe8eca217e8421
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2024-01-14 17:53:53 +0700

    Create corpobj.js

---
+++ corp/lib/corpobj.js
@@ -0,0 +1,113 @@
+//Credits to @CatLover for making Corporation library script accessible to everyone with
+//an in-depth Corporation guide document, most of these are just me trying to translate
+//some of code to javascript since CatLover use typescript with some modification
+
+export const cities = ["Sector-12", "Aevum", "Chongqing", "New Tokyo", "Ishima", "Volhaven"]
+
+export const citiesobj = {
+  Sector_12: "Sector-12",
+  Aevum: "Aevum",
+  Chongqing: "Chongqing",
+  New_Tokyo: "New Tokyo",
+  Ishima: "Ishima",
+  Volhaven: "Volhaven"
+}
+
+export const corpstate = {
+  Start: "START",
+  Purchase: "PURCHASE",
+  Production: "PRODUCTION",
+  Export: "EXPORT",
+  Sale: "SALE"
+}
+
+export const industry = {
+  WATER_UTILITIES: "Water Utilities",
+  SPRING_WATER: "Spring Water",
+  AGRICULTURE: "Agriculture",
+  FISHING: "Fishing",
+  MINING: "Mining",
+  REFINERY: "Refinery",
+  RESTAURANT: "Restaurant",
+  TOBACCO: "Tobacco",
+  CHEMICAL: "Chemical",
+  PHARMACEUTICAL: "Pharmaceutical",
+  COMPUTER_HARDWARE: "Computer Hardware",
+  ROBOTICS: "Robotics",
+  SOFTWARE: "Software",
+  HEALTHCARE: "Healthcare",
+  REAL_ESTATE: "Real Estate",
+}
+
+export const researchName = {
+  HI_TECH_RND_LABORATORY: "Hi-Tech R&D Laboratory",
+  AUTO_BREW: "AutoBrew",
+  AUTO_PARTY: "AutoPartyManager",
+  AUTO_DRUG: "Automatic Drug Administration",
+  CPH4_INJECT: "CPH4 Injections",
+  DRONES: "Drones",
+  DRONES_ASSEMBLY: "Drones - Assembly",
+  DRONES_TRANSPORT: "Drones - Transport",
+  GO_JUICE: "Go-Juice",
+  HR_BUDDY_RECRUITMENT: "HRBuddy-Recruitment",
+  HR_BUDDY_TRAINING: "HRBuddy-Training",
+  MARKET_TA_1: "Market-TA.I",
+  MARKET_TA_2: "Market-TA.II",
+  OVERCLOCK: "Overclock",
+  SELF_CORRECTING_ASSEMBLERS: "Self-Correcting Assemblers",
+  STIMU: "Sti.mu",
+  UPGRADE_CAPACITY_1: "uPgrade: Capacity.I",
+  UPGRADE_CAPACITY_2: "uPgrade: Capacity.II",
+  UPGRADE_DASHBOARD: "uPgrade: Dashboard",
+  UPGRADE_FULCRUM: "uPgrade: Fulcrum",
+}
+
+export const upgradeName = {
+  SMART_FACTORIES: "Smart Factories",
+  SMART_STORAGE: "Smart Storage",
+  DREAM_SENSE: "DreamSense",
+  WILSON_ANALYTICS: "Wilson Analytics",
+  NUOPTIMAL_NOOTROPIC_INJECTOR_IMPLANTS: "Nuoptimal Nootropic Injector Implants",
+  SPEECH_PROCESSOR_IMPLANTS: "Speech Processor Implants",
+  NEURAL_ACCELERATORS: "Neural Accelerators",
+  FOCUS_WIRES: "FocusWires",
+  ABC_SALES_BOTS: "ABC SalesBots",
+  PROJECT_INSIGHT: "Project Insight"
+}
+
+export const unlockName = {
+  EXPORT: "Export",
+  SMART_SUPPLY: "Smart Supply",
+  MARKET_RESEARCH_DEMAND: "Market Research - Demand",
+  MARKET_DATA_COMPETITION: "Market Data - Competition",
+  VE_CHAIN: "VeChain",
+  SHADY_ACCOUNTING: "Shady Accounting",
+  GOVERNMENT_PARTNERSHIP: "Government Partnership",
+  WAREHOUSE_API: "Warehouse API",
+  OFFICE_API: "Office API"
+}
+
+export const employeePosition = {
+  OPERATIONS: "Operations",
+  ENGINEER: "Engineer",
+  BUSINESS: "Business",
+  MANAGEMENT: "Management",
+  RESEARCH_DEVELOPMENT: "Research & Development",
+  INTERN: "Intern",
+  UNASSIGNED: "Unassigned"
+}
+
+export const materialName = {
+  MINERALS: "Minerals",
+  ORE: "Ore",
+  WATER: "Water",
+  FOOD: "Food",
+  PLANTS: "Plants",
+  METAL: "Metal",
+  HARDWARE: "Hardware",
+  CHEMICALS: "Chemicals",
+  DRUGS: "Drugs",
+  ROBOTS: "Robots",
+  AI_CORES: "AI Cores",
+  REAL_ESTATE: "Real Estate"
+}
