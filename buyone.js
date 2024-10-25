679d6eed4f5c42a2979e79650ac133f43388e971
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2024-10-25 18:05:32 +0700

    Update buyone.js

--- buyone.js
+++ buyone.js
@@ -3,14 +3,21 @@ import { getserver } from "lib/function.js"
 export async function main(ns) {
   const ram = ns.args[0] ?? 32768
   const name = "pserv"
-  let buyram = Math.pow(2 ,Math.ceil(Math.log2(ram)))
+  let buyram = Math.pow(2, Math.ceil(Math.log2(ram)))
+  let cost = ns.getPurchasedServerCost(buyram)
   let servers = getserver(ns)
   let pserver = servers.filter(x => x.startsWith(name))
-  if (pserver.lenght == 4) {
+  ns.tprint(pserver)
+  if (cost > ns.getServerMoneyAvailable("home")) {
+    throw new Error(`Not enough money to buy server!, require ${ns.formatNumber(cost, 1, 1e4)} for ${ns.formatRam(buyram)}`)
+  }
+
+  if (pserver.lenght >= 4) {
     for (let i = 0; i < pserver.length; i++) {
       ns.deleteServer(pserver[i])
     }
   }
+
   ns.tprint(`Purchasing server for ${ns.formatRam(buyram)}`)
   ns.purchaseServer(name, buyram)
 }
