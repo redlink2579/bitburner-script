6e3168bded0488691f49f89178a2e32e5b8a9640
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2025-03-29 17:54:27 +0700

    Create grow.js

---
+++ Proto/Relapse1/grow.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  const works = JSON.parse(ns.args[0])
+  const port = ns.getPortHandle(ns.pid)
+  let delay = works.end - works.time - Date.now()
+  ns.print(ns.tFormat(delay))
+
+  if (delay < 0) {
+    ns.tprint(`WARN: Batch ${works.batch} ${works.job} was ${-delay}ms late, expect running time was:, ${ns.tFormat(works.end - Date.now())}`)
+    port.write(-delay)
+    delay = 0
+  } else {
+    ns.tprint(`INFO: Running ${works.job} at ${works.target} for: ${ns.tFormat(works.end + delay - Date.now())}`)
+    port.write(0)
+  }
+  let growadd = await ns.grow(works.target, { additionalMsec: delay })
+  ns.write("logging.txt", "grow-add = " + growadd + " \n ", "a")
+
+  ns.atExit(() => {
+    if (works.report) { ns.writePort(works.port, works.type + works.host) }
+    ns.tprint(`SUCCESS: Batch ${works.batch} : ${works.job} has finished at  ${new Date().toLocaleTimeString()}`)
+  })
+}
