0b68744ecebaa1275d4c19110a0a187c0ebf4398
redlink2579 <70947799+redlink2579@users.noreply.github.com>
2026-02-14 19:11:07 +0700

    Update module.js

--- Batcher/libs/module.js
+++ Batcher/libs/module.js
@@ -3,10 +3,13 @@ export async function main(ns) {
   ns.tprint(`This is module library for my TUI script, You are not supposed to run this`)
 }
 
+/**Note to self:If you going to add more module make sure it's include all info 
+ * even if you are not using it shit will break if you don't include it*/
+
 /** @param {NS} ns */
-export function lazy(ns, thedot, dot, timer) {
+export function lazy(ns, info, dot, timer = 0) {
   ns.print(`======================================`);
-  ns.print(`Nothing happening${thedot[dot]}`);
+  ns.print(`Nothing happening${dot.thedot[dot.index]}`);
   ns.print(`--------------------------------------`);
   ns.print(`Please consult the graph`);
   ns.print(`             │                        `);
@@ -14,7 +17,7 @@ export function lazy(ns, thedot, dot, timer) {
   ns.print(`             │                        `);
   ns.print(`Things that  │                        `);
   ns.print(`   happen    │                        `);
-  ns.print(`             │                        `); 
+  ns.print(`             │                        `);
   ns.print(`          0 ─┼────────────────────────`);
   ns.print(`             │                        `);
   ns.print(`         ────┼────────────────────────`);
@@ -26,27 +29,27 @@ export function lazy(ns, thedot, dot, timer) {
 }
 
 /** @param {NS} ns */
-export function proto(ns, mode, target, data, yoink, timer, thedot, dot) {
+export function proto(ns, info, dot, timer = 0) {
   ns.print(`======================================`);
-  switch (mode) {
+  switch (info.mode) {
     case 0:
-      ns.print(`Nothing happening${thedot[dot]}`);
+      ns.print(`Nothing happening${dot.thedot[dot.index]}`);
       break;
     case 1:
-      ns.print(`Running Batch on ${target}${thedot[dot]}`);
+      ns.print(`Running Batch on ${info.target}${dot.thedot[dot.index]}`);
       break;
     case 2:
-      ns.print(`Preparing ${target} for batching${thedot[dot]}`);
+      ns.print(`Preparing ${info.target} for batching${dot.thedot[dot.index]}`);
   }
   ns.print(`--------------------------------------`);
-  if (target === `none`) {
-    ns.print(`Target not found${thedot[dot]}`)
+  if (info.target === `none`) {
+    ns.print(`Target not found${dot.thedot[dot.index]}`)
   }
   else {
-    ns.print(`Target: ${data.target}`)
-    ns.print(`┣━ Money: ${ns.formatNumber(data.money, 2, 1e3, true)}\$ / ${ns.formatNumber(data.maxMoney, 2, 1e3, true)}\$`)
-    ns.print(`┃  ┗━ Actual gain: ${ns.formatNumber((data.maxMoney * yoink) * ns.getPlayer().mults.hacking_money)}\$ (${ns.formatPercent(yoink, 2)})`)
-    ns.print(`┗━ Security: + ${(data.sec - data.minSec).toFixed(2)}`)
+    ns.print(`Target: ${info.target}`)
+    ns.print(`┣━ Money: ${ns.formatNumber(info.data.money, 2, 1e3, true)}\$ / ${ns.formatNumber(info.data.maxMoney, 2, 1e3, true)}\$`)
+    ns.print(`┃  ┗━ Actual gain: ${ns.formatNumber(((info.data.maxMoney * info.yoink) * ns.getBitNodeMultipliers().ScriptHackMoney) * (ns.getPlayer().mults.hacking_money * ns.getBitNodeMultipliers().ScriptHackMoneyGain))}\$ (${ns.formatPercent(info.yoink, 0)})`)
+    ns.print(`┗━ Security: + ${(info.data.sec - info.data.minSec).toFixed(2)}`)
     //ns.print(`┗━ Batch count:`)
   }
   ns.print(`Runtime:${ns.tFormat(timer)}`)
